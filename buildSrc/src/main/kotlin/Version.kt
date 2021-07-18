f1ebb7f3cf350895761ecadf31955ac681184f15
zak <linhenji@163.com>
2021-07-18 23:07:36 +0800

    feat: 添加 buildSrc 目录

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,13 @@
+/**
+ * 版本号信息记录
+ */
+object Version {
+
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.3"
+    const val mindSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+}
