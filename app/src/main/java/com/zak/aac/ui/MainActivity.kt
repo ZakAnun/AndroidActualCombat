77046eec13e1318d96101d344c6a54829fd6e2eb
zak <linhenji@163.com>
2022-03-07 08:28:53 +0800

    feat: 添加网络阶段回调

--- app/src/main/java/com/zak/aac/ui/MainActivity.kt
+++ app/src/main/java/com/zak/aac/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.zak.aac.R
+import okhttp3.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,6 +14,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.io.IOException
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 
 /**
@@ -32,10 +37,147 @@ class MainActivity: AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val okClient = OkHttpClient.Builder().eventListener(object : EventListener() {
+
+            override fun callStart(call: okhttp3.Call) {
+                Log.d(TAG, "callStart: ")
+            }
+
+            override fun proxySelectStart(call: okhttp3.Call, url: HttpUrl) {
+                Log.d(TAG, "proxySelectStart: ")
+            }
+
+            override fun proxySelectEnd(call: okhttp3.Call, url: HttpUrl, proxies: List<Proxy>) {
+                Log.d(TAG, "proxySelectEnd: ")
+            }
+
+            override fun dnsStart(call: okhttp3.Call, domainName: String) {
+                Log.d(TAG, "dnsStart: ")
+            }
+
+            override fun dnsEnd(
+                call: okhttp3.Call,
+                domainName: String,
+                inetAddressList: List<InetAddress>
+            ) {
+                Log.d(TAG, "dnsEnd: ")
+            }
+
+            override fun connectStart(
+                call: okhttp3.Call,
+                inetSocketAddress: InetSocketAddress,
+                proxy: Proxy
+            ) {
+                Log.d(TAG, "connectStart: ")
+            }
+
+            override fun secureConnectStart(call: okhttp3.Call) {
+                Log.d(TAG, "secureConnectStart: ")
+            }
+
+            override fun secureConnectEnd(call: okhttp3.Call, handshake: Handshake?) {
+                Log.d(TAG, "secureConnectEnd: ")
+            }
+
+            override fun connectEnd(
+                call: okhttp3.Call,
+                inetSocketAddress: InetSocketAddress,
+                proxy: Proxy,
+                protocol: Protocol?
+            ) {
+                Log.d(TAG, "connectEnd: ")
+            }
+
+            override fun connectFailed(
+                call: okhttp3.Call,
+                inetSocketAddress: InetSocketAddress,
+                proxy: Proxy,
+                protocol: Protocol?,
+                ioe: IOException
+            ) {
+                Log.d(TAG, "connectFailed: ")
+            }
+
+            override fun connectionAcquired(call: okhttp3.Call, connection: Connection) {
+                Log.d(TAG, "connectionAcquired: ")
+            }
+
+            override fun connectionReleased(call: okhttp3.Call, connection: Connection) {
+                Log.d(TAG, "connectionReleased: ")
+            }
+
+            override fun requestHeadersStart(call: okhttp3.Call) {
+                Log.d(TAG, "requestHeadersStart: ")
+            }
+
+            override fun requestHeadersEnd(call: okhttp3.Call, request: Request) {
+                Log.d(TAG, "requestHeadersEnd: ")
+            }
+
+            override fun requestBodyStart(call: okhttp3.Call) {
+                Log.d(TAG, "requestBodyStart: ")
+            }
+
+            override fun requestBodyEnd(call: okhttp3.Call, byteCount: Long) {
+                Log.d(TAG, "requestBodyEnd: ")
+            }
+
+            override fun requestFailed(call: okhttp3.Call, ioe: IOException) {
+                Log.d(TAG, "requestFailed: ")
+            }
+
+            override fun responseHeadersStart(call: okhttp3.Call) {
+                Log.d(TAG, "responseHeadersStart: ")
+            }
+
+            override fun responseHeadersEnd(call: okhttp3.Call, response: okhttp3.Response) {
+                Log.d(TAG, "responseHeadersEnd: ")
+            }
+
+            override fun responseBodyStart(call: okhttp3.Call) {
+                Log.d(TAG, "responseBodyStart: ")
+            }
+
+            override fun responseBodyEnd(call: okhttp3.Call, byteCount: Long) {
+                Log.d(TAG, "responseBodyEnd: ")
+            }
+
+            override fun responseFailed(call: okhttp3.Call, ioe: IOException) {
+                Log.d(TAG, "responseFailed: ")
+            }
+
+            override fun callEnd(call: okhttp3.Call) {
+                Log.d(TAG, "callEnd: ")
+            }
+
+            override fun callFailed(call: okhttp3.Call, ioe: IOException) {
+                Log.d(TAG, "callFailed: ")
+            }
+
+            override fun satisfactionFailure(call: okhttp3.Call, response: okhttp3.Response) {
+                Log.d(TAG, "satisfactionFailure: ")
+            }
+
+            override fun cacheHit(call: okhttp3.Call, response: okhttp3.Response) {
+                Log.d(TAG, "cacheHit: ")
+            }
+
+            override fun cacheMiss(call: okhttp3.Call) {
+                Log.d(TAG, "cacheMiss: ")
+            }
+
+            override fun cacheConditionalHit(call: okhttp3.Call, cachedResponse: okhttp3.Response) {
+                Log.d(TAG, "cacheConditionalHit: ")
+            }
+
+
+        })
+
         val retrofit = Retrofit.Builder()
 //            .baseUrl("https://www.zakli.cn/")
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okClient.build())
             .build()
 
         val apiService = retrofit.create(ApiService::class.java)
